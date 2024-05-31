45e22ab7c061ff7d63a6e542afab3e25a84dddd3
bamjun <khy5116@naver.com>
2024-05-31 18:17:33 +0900

    fix:url

--- django/users/views.py
+++ django/users/views.py
@@ -131,7 +131,7 @@ class KakaoCallBackView(APIView):
             # 쿠키에 토큰 저장 (세션 쿠키로 설정)
             # response = HttpResponseRedirect('') # 로그인 완료 시 리디렉션할 URL
             # response = HttpResponseRedirect('http://localhost:8000/users/myinfo') # 로그인 완료 시 리디렉션할 URL
-            response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile/') # 로그인 완료 시 리디렉션할 URL
+            response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile') # 로그인 완료 시 리디렉션할 URL
             response.set_cookie('access_token', str(refresh.access_token), httponly=True, samesite='None', secure=True)
             response.set_cookie('refresh_token', str(refresh), httponly=True, samesite='None', secure=True)
             response.set_cookie('user_state', str("TRUE"), httponly=True, samesite='None', secure=True)
