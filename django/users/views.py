713ec4bb9e06ecbe80d2a17e42a791b1eb96093b
bamjun <khy5116@naver.com>
2024-05-31 19:13:12 +0900

    fix:url

--- django/users/views.py
+++ django/users/views.py
@@ -76,8 +76,7 @@ class MyInfoView(APIView):
 class KakaoView(APIView):
     def get(self, request):
         kakao_api = 'https://kauth.kakao.com/oauth/authorize?response_type=code'
-        # redirect_uri = 'https://api.oz-02-main-04.xyz/api/v1/users/kakao/callback'
-        redirect_uri = 'https://www.oz-02-main-04.xyz/profile'
+        redirect_uri = 'https://api.oz-02-main-04.xyz/api/v1/users/kakao/callback'
         # redirect_uri = 'http://localhost:8000/users/kakao/callback'
         client_id = '92ec542f65f17550dbc2fbf553c44822'
 
@@ -134,7 +133,7 @@ class KakaoCallBackView(APIView):
             # response = HttpResponseRedirect('http://localhost:8000/users/myinfo') # 로그인 완료 시 리디렉션할 URL
             # response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile') # 로그인 완료 시 리디렉션할 URL
             # response = HttpResponseRedirect('https://api.oz-02-main-04.xyz/api/v1/users/myinfo') # 로그인 완료 시 리디렉션할 URL
-            response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/login') # 로그인 완료 시 리디렉션할 URL
+            response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile') # 로그인 완료 시 리디렉션할 URL
             response.set_cookie('access_token', str(refresh.access_token), httponly=True, samesite='Lax', secure=True)
             response.set_cookie('refresh_token', str(refresh), httponly=True, samesite='Lax', secure=True)
             response.set_cookie('user_state', str("TRUE"), httponly=True, samesite='Lax', secure=True)
