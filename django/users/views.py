379c8037417a2db2c905cc95b2519d2f16cc6cb7
kyoorochi <pmh0521@gmail.com>
2024-06-04 14:56:46 +0900

    닉네임필드 추가, 정보열람부분 일부 수정 - 마이그레이션 다시 해야됨

--- django/users/views.py
+++ django/users/views.py
@@ -106,8 +106,8 @@ class MyInfoView(APIView):
 class KakaoView(APIView):
     def get(self, request):
         kakao_api = "https://kauth.kakao.com/oauth/authorize?response_type=code"
-        # redirect_uri = "https://api.oz-02-main-04.xyz/api/v1/users/kakao/callback"
-        redirect_uri = 'http://localhost:8000/api/v1/users/kakao/callback'
+        redirect_uri = "https://api.oz-02-main-04.xyz/api/v1/users/kakao/callback"
+        # redirect_uri = 'http://localhost:8000/api/v1/users/kakao/callback'
         client_id = "92ec542f65f17550dbc2fbf553c44822"
 
         return redirect(
@@ -119,8 +119,8 @@ class KakaoCallBackView(APIView):
         data = {
             "grant_type": "authorization_code",
             "client_id": "92ec542f65f17550dbc2fbf553c44822",
-            # "redirection_uri": "https://api.oz-02-main-04.xyz/api/v1/users/kakao/",
-            'redirection_uri' : 'http://localhost:8000/api/v1/users/kakao/',
+            "redirection_uri": "https://api.oz-02-main-04.xyz/api/v1/users/kakao/",
+            # 'redirection_uri' : 'http://localhost:8000/api/v1/users/kakao/',
             "code": request.GET["code"],
             "client_secret": "qdl4Hfn7QhS2H9l2aKiYFJdGwpkeGcc1",
         }
@@ -165,8 +165,8 @@ class KakaoCallBackView(APIView):
             refresh = RefreshToken.for_user(user)
 
             # 쿠키에 토큰 저장 (세션 쿠키로 설정)
-            # response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile') # 로그인 완료 시 리디렉션할 URL
-            response = HttpResponseRedirect('http://localhost:8000/api/v1/users/') # 로그인 완료 시 리디렉션할 URL
+            response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile') # 로그인 완료 시 리디렉션할 URL
+            # response = HttpResponseRedirect('http://localhost:8000/api/v1/users/') # 로그인 완료 시 리디렉션할 URL
 
             # 배포 환경에서만 secure=True와 samesite='None' 설정
             secure_cookie = request.is_secure()
