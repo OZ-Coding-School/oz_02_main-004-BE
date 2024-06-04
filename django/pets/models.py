37da9660c740823616ef3e370e6cc186fead68ab
bamjun <khy5116@naver.com>
2024-06-04 18:16:57 +0900

    Feat:signal

--- django/pets/models.py
+++ django/pets/models.py
@@ -1,17 +1,27 @@
 from django.db import models
+from common.models import CommonModel
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Pet(models.Model):
+class Pet(CommonModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pets', verbose_name='사용자 ID')
     point = models.IntegerField(verbose_name='경험치', default=0)
-    level = models.IntegerField(verbose_name='레벨', default=1)
     name = models.CharField(max_length=50, verbose_name='이름')
-    status = models.CharField(max_length=50, verbose_name='상태')
+    hunger_degree = models.IntegerField(default=0, verbose_name='배고픔 정도')
     type = models.CharField(max_length=50, verbose_name='캐릭터 종류')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성 시간')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정 시간')
+    random_boxs = models.models.IntegerField(_(""))아이템 랜덤으로 나오는 박스 갯수
+    
+
+
 
     def __str__(self):
         return self.name
+
+
+class Pet_rating(CommonModel):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='ratings', verbose_name='사용자 ID')
+    rating_type = models. 펫의 레벨이나, 배고픔 정도를 둘중하나선택.
+    level = models.IntegerField
+    point = model.PositiveIntegerField 
+    
