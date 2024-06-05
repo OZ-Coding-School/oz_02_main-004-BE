ff2ff34ca6c7c842071e93cdf8241ba252ebba21
bamjun <khy5116@naver.com>
2024-06-05 18:29:03 +0900

    Fix:print type[D[D[D[Demail type

--- django/users/views.py
+++ django/users/views.py
@@ -158,6 +158,8 @@ class KakaoCallBackView(APIView):
 
             # 사용자가 존재하지 않으면 새 사용자 생성
             except User.DoesNotExist:
+                print(email)
+                print(generate_random_nickname)
                 serializer = CreateUserSerializer(data={"email": email, "nickname": generate_random_nickname})
                 if serializer.is_valid():
                     user = serializer.save()
