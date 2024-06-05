eafbc71f9ca8a1f97646083b0c3453c79607573d
bamjun <khy5116@naver.com>
2024-06-05 11:46:24 +0900

    Chore:working model

--- django/pets/models.py
+++ django/pets/models.py
@@ -25,3 +25,16 @@ class Pet_rating(CommonModel):
     level = models.IntegerField
     point = model.PositiveIntegerField 
     
+"""
+bigint	id		테이블 ID	Primary Key	간식 정보 관리
+int	type		종류(0:밥, 1:간식)	Not Null	
+varchar(10)	item_name		아이템 이름	Not Null	
+datetime(6)	created_at		생성 시간	Not Null	
+datetime(6)	updated_at		수정 시간	Not Null	
+bigint	pet_id		pet ID	Foreign Key	pets 테이블과의 관계 설정
+"""
+class Snacks(CommonModel):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='snacks', verbose_name='사용자 ID')
+    type = models.CharField(max_length=50, verbose_name='캐릭터 종류')
+    item_name = models.CharField(max_length=10)
+    
