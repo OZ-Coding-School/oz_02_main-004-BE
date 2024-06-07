a8fb613f59d672497ec9d39ab161c4481e8128fa
ark0723 <ark0723@gmail.com>
2024-06-07 18:56:03 +0900

    update seriazlier for swagger body field

--- django/posts/serializer.py
+++ django/posts/serializer.py
@@ -1,64 +1,93 @@
-from rest_framework.serializers import ModelSerializer, ValidationError
+from rest_framework import serializers
 from posts.models import Post, Timer, Music, ToDo
 from users.serializers import UserSerializer
 from django.utils import timezone
-from rest_framework import serializers
 
-class ToDoSerializer(ModelSerializer):
+
+class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
-        fields = '__all__'
+        fields = "__all__"
 
-class ToDoCreateSerializer(ModelSerializer):
+
+class ToDoCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
-        fields = ('todo_item',)
+        fields = ("todo_item",)
 
-class PostSerializer(ModelSerializer):
+
+class ToDoEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = ("todo_item", "done")
+
+
+class PostSerializer(serializers.ModelSerializer):
     # user = UserSerializer(read_only=True)
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = "__all__"
         # depth = 1
 
-class PostCreateSerializer(ModelSerializer):
-    feeling_status = serializers.ChoiceField(choices=[0, 1, 2], required=False, default=0)
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    feeling_status = serializers.ChoiceField(
+        choices=[0, 1, 2], required=False, default=0
+    )
 
     class Meta:
         model = Post
-        fields = ('feeling_status', 'todo_date', 'memo')
+        fields = ("feeling_status", "todo_date", "memo")
 
     # 새로 생성되는 todo_date는 today 이전 날짜로는 생성되지 않는다
     def validate_todo_date(self, value):
         if value < timezone.now().date():
-            raise ValidationError('todo date cannot be in the past.')
-        
-        user = self.context.get('user_id')
+            raise serializers.ValidationError("todo date cannot be in the past.")
+
+        user = self.context.get("user_id")
         print(user)
         # user = self.context['request'].user
         if Post.objects.filter(todo_date=value, user=user).exists():
-            raise ValidationError('You already have a post for this date.')
+            raise serializers.ValidationError("You already have a post for this date.")
         return value
 
 
+class PostDeleteSerializer(serializers.ModelSerializer):
+    # user = UserSerializer(read_only=True)
+    class Meta:
+        model = Post
+        fields = ("todo_date",)
 
 
-class SpotifySerializer(ModelSerializer):
+class SpotifySerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields = '__all__'
+        fields = "__all__"
 
-class SongCreateSerializer(ModelSerializer):
+
+class SongCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
-        fields = ('singer', 'album', 'title', 'release_date', 'song_url')
+        fields = ("singer", "album", "title", "release_date", "song_url")
 
-class TimerSerializer(ModelSerializer):
+
+class SpotifyQuerySerializer(serializers.Serializer):
+    query = serializers.CharField(required=True, max_length=255)
+
+
+class TimerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timer
-        fields = '__all__'
+        fields = "__all__"
 
-class TimerCreateSerializer(ModelSerializer):
+
+class TimerCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timer
-        fields = ('on_btn', 'start', 'end', 'duration')
+        fields = ("on_btn", "start", "end", "duration")
+
+
+class TimerActionSerializer(serializers.Serializer):
+    action = serializers.ChoiceField(
+        choices=["pause", "restart", "reset"], required=True
+    )
--- django/posts/views.py
+++ django/posts/views.py
@@ -12,11 +12,15 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from posts.serializer import (
     PostSerializer,
     PostCreateSerializer,
+    PostDeleteSerializer,
     SpotifySerializer,
+    SpotifyQuerySerializer,
     SongCreateSerializer,
     TimerSerializer,
     TimerCreateSerializer,
+    TimerActionSerializer,
     ToDoSerializer,
+    ToDoEditSerializer,
     ToDoCreateSerializer,
 )
 from drf_yasg.utils import swagger_auto_schema
@@ -68,7 +72,9 @@ class PostsByUser(APIView):
     @swagger_auto_schema(request_body=PostCreateSerializer)
     def post(self, request, user_id):
         user = self.get_user(user_id=user_id)
-        serializer = PostCreateSerializer(data=request.data, context={'user_id': user_id})
+        serializer = PostCreateSerializer(
+            data=request.data, context={"user_id": user_id}
+        )
         if serializer.is_valid():
             post = serializer.save(user=user)
             serializer = PostSerializer(post)
@@ -76,7 +82,7 @@ class PostsByUser(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # 포스트 내용 변경 : 'todo_date' is required
-    @swagger_auto_schema(request_body=PostSerializer)
+    @swagger_auto_schema(request_body=PostCreateSerializer)
     def put(self, request, user_id):
         target_date = request.data.get("todo_date")
         post = self.get_post(user_id=user_id, target_date=target_date)
@@ -93,6 +99,7 @@ class PostsByUser(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # 포스트 삭제 : 'todo_date' is required
+    @swagger_auto_schema(request_body=PostDeleteSerializer)
     def delete(self, request, user_id):
         target_date = request.data.get("todo_date")
         post = self.get_post(user_id=user_id, target_date=target_date)
@@ -113,7 +120,7 @@ class ToDoView(APIView):
 
     def get_post(self, post_id):
         return get_object_or_404(Post, id=post_id)
-    
+
     @swagger_auto_schema(request_body=ToDoCreateSerializer)
     def post(self, request, post_id):
         post = self.get_post(post_id)
@@ -140,8 +147,8 @@ class ToDoEdit(APIView):
 
     def get_post(self, post_id):
         return get_object_or_404(Post, id=post_id)
-    
-    @swagger_auto_schema(request_body=ToDoSerializer)
+
+    @swagger_auto_schema(request_body=ToDoEditSerializer)
     def put(self, request, post_id, todo_id):
         post = self.get_post(post_id)
         try:
@@ -193,8 +200,8 @@ class Spotify(APIView):
     def get_current_song(self, post_id):
         post = self.get_post(post_id=post_id)
         return post.musics.first()
-    
-    @swagger_auto_schema(query_serializer=SpotifySerializer)
+
+    @swagger_auto_schema(query_serializer=SpotifyQuerySerializer)
     def get(self, request, post_id):
         # get songs' list from searched results (max: 50 songs)
         query = request.data.get("query", None)
@@ -229,7 +236,7 @@ class Spotify(APIView):
             return Response(
                 {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
-        
+
     @swagger_auto_schema(request_body=SongCreateSerializer)
     def post(self, request, post_id):
         # assumption: a certain song is selected
@@ -252,7 +259,7 @@ class Spotify(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # 등록된 음악을 변경할 경우
-    @swagger_auto_schema(request_body=SpotifySerializer)
+    @swagger_auto_schema(request_body=SongCreateSerializer)
     def put(self, request, post_id):
         current_song = self.get_current_song(post_id)
         if not current_song:
@@ -295,7 +302,6 @@ class TimerView(APIView):
         timer.update_duration()
         serializer = TimerSerializer(timer)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
 
     def post(self, request, post_id):
         post = self.get_post(post_id)
@@ -307,7 +313,7 @@ class TimerView(APIView):
         return Response(serializer.errors, status=status.HTTP_200_BAD_REQUEST)
 
     # timer reset/pause/restart
-    @swagger_auto_schema(request_body=SpotifySerializer)
+    @swagger_auto_schema(request_body=TimerActionSerializer)
     def patch(self, request, post_id):
         action = request.data.get("action", None)
         post = self.get_post(post_id)
