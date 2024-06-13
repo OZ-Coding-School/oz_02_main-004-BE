70cf03d7fc7f65eb342c3a8ca28e4cfe1c8150e2
kyoorochi <pmh0521@gmail.com>
2024-06-13 18:26:27 +0900

    FE 로컬개발용으로 전환

--- django/users/kakao.py
+++ django/users/kakao.py
@@ -23,8 +23,9 @@ class KakaoView(APIView):
     )
     def get(self, request):
         kakao_api = 'https://kauth.kakao.com/oauth/authorize?response_type=code'
-        redirect_uri = 'https://api.oz-02-main-04.xyz/api/v1/users/kakao/callback'
+        # redirect_uri = 'https://api.oz-02-main-04.xyz/api/v1/users/kakao/callback'
         # redirect_uri = 'http://localhost:8000/api/v1/users/kakao/callback'
+        redirect_uri = 'http://localhost:3000/api/v1/users/kakao/callback'
         client_id = '92ec542f65f17550dbc2fbf553c44822'
         return redirect(f'{kakao_api}&client_id={client_id}&redirect_uri={redirect_uri}')
 
@@ -34,8 +35,9 @@ class KakaoCallBackView(APIView):
         data = {
             'grant_type': 'authorization_code',
             'client_id': '92ec542f65f17550dbc2fbf553c44822',
-            'redirection_uri': 'https://api.oz-02-main-04.xyz/api/v1/users/kakao/',
+            # 'redirection_uri': 'https://api.oz-02-main-04.xyz/api/v1/users/kakao/',
             # 'redirection_uri' : 'http://localhost:8000/api/v1/users/kakao/',
+            'redirection_uri': 'https://localhost:3000',
             'code': request.GET['code'],
             'client_secret': 'qdl4Hfn7QhS2H9l2aKiYFJdGwpkeGcc1',
         }
@@ -77,8 +79,9 @@ class KakaoCallBackView(APIView):
             refresh = RefreshToken.for_user(user)
 
             # 쿠키에 토큰 저장 (세션 쿠키로 설정)
-            response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile') # 로그인 완료 시 리디렉션할 URL
+            # response = HttpResponseRedirect('https://www.oz-02-main-04.xyz/profile') # 로그인 완료 시 리디렉션할 URL
             # response = HttpResponseRedirect('http://localhost:8000/api/v1/users/myinfo')
+            response = HttpResponseRedirect('http://localhost:3000/')
 
             # CSRF 토큰 설정
             csrf_token_value = get_token(request)
