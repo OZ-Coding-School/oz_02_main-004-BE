a3fd9acf1a7e1088c6cd4c94b228b2ded98436af
ark0723 <ark0723@gmail.com>
2024-06-14 14:04:30 +0900

    Pet: random box api done

--- django/pets/urls.py
+++ django/pets/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [path('mypet/<int:user_id>/', MyPetView.as_view(), name='mypet'),]
+urlpatterns = [
+    path("mypet/<int:user_id>/", MyPetView.as_view(), name="mypet"),
+    path(
+        "open-random-box/<int:user_id>/",
+        OpenRandomBoxView.as_view(),
+        name="open_random_box",
+    ),
+]
