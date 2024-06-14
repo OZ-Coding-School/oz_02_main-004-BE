d8bbb566448c93ca13665be51b299730df6c4015
bamjun <khy5116@naver.com>
2024-06-14 14:17:28 +0900

    test

--- django/pets/serializers.py
+++ django/pets/serializers.py
@@ -1,6 +1,6 @@
 # pets/serializers.py
 from rest_framework import serializers
-from .models import Pet, Accessory, Background, PetCollection, Snack, SnackType
+from .models import Pet, Accessory, Background, PetCollection, Snack, SnackType, PetRating
 
 class AccessorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,9 +17,15 @@ class PetCollectionSerializer(serializers.ModelSerializer):
         model = PetCollection
         fields = ['pet_name', 'image']
 
+class PetratingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PetRating
+        fields = ['level', 'point']
+
 class PetSerializer(serializers.ModelSerializer):
     primary_accessory = AccessorySerializer()
     primary_background = BackgroundSerializer()
+    pet_rating = PetratingSerializer()
     primary_pet = PetCollectionSerializer()
     rice_quantity = serializers.SerializerMethodField()
     snack_quantity = serializers.SerializerMethodField()
@@ -34,6 +40,15 @@ class PetSerializer(serializers.ModelSerializer):
         snack_type = SnackType.objects.get(name='snack')
         snack_snack = Snack.objects.filter(pet=obj, snack_type=snack_type).first()
         return snack_snack.quantity if snack_snack else 0
+    
+    class Meta:
+        model = Pet
+        fields = [
+            'user', 'point', 'hunger_degree', 'random_boxes', 'pet_rating',
+            'primary_accessory', 'primary_background', 'primary_pet',
+            'rice_quantity', 'snack_quantity', 'active_pet'
+        ]
+
     
 class PetMainSerializer(PetSerializer):
     class Meta:
