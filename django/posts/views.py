339b0b7f5b2ff503d5fb346bdcfaa2af7ca1022d
kyoorochi <pmh0521@gmail.com>
2024-06-20 01:37:11 +0900

    fix

--- django/posts/views.py
+++ django/posts/views.py
@@ -8,21 +8,9 @@ from rest_framework.exceptions import NotFound
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from posts.serializer import (
-    PostSerializer,
-    PostCreateSerializer,
-    PostDeleteSerializer,
-    SpotifySerializer,
-    SongCreateSerializer,
-    SpotifyQuerySerializer,
-    SpotifySearchSerializer,
-    TimerSerializer,
-    TimerCreateSerializer,
-    TimerActionSerializer,
-    ToDoSerializer,
-    ToDoEditSerializer,
-    ToDoCreateSerializer,
-    ConsecutiveDaysSerializer,
-    UserGoalSerializer,
+    PostSerializer, PostCreateSerializer, PostDeleteSerializer, SpotifySerializer, SongCreateSerializer, SpotifyQuerySerializer,
+    SpotifySearchSerializer, TimerSerializer, TimerCreateSerializer, TimerActionSerializer, ToDoSerializer, ToDoEditSerializer,
+    ToDoCreateSerializer, ConsecutiveDaysSerializer, UserGoalSerializer,
 )
 from drf_yasg.utils import swagger_auto_schema
 
@@ -43,19 +31,19 @@ class UserGoalView(APIView):
         return request.user
 
     def get(self, request):
-        """
+        '''
         Retrieve the goal for the authenticated user.
-        """
+        '''
         user = self.get_user(request)
-        goal = getattr(user, "goal", None)
+        goal = getattr(user, 'goal', None)
         serializer = UserGoalSerializer(goal)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(request_body=UserGoalSerializer)
     def post(self, request):
-        """
+        '''
         Create or update the goal for the authenticated user.
-        """
+        '''
         user = self.get_user(request)
         goal, _ = UserGoal.objects.get_or_create(user=user)
         serializer = UserGoalSerializer(goal, data=request.data, partial=True)
@@ -75,12 +63,12 @@ class CalendarView(APIView):
         return request.user
 
     def get(self, request):
-        """
+        '''
         Retrieve the streak of consecutive successful days.
-        """
+        '''
         user = self.get_user(request)
         streak = get_consecutive_success_days(user)
-        serializer = ConsecutiveDaysSerializer(data={"streak": streak})
+        serializer = ConsecutiveDaysSerializer(data={'streak': streak})
 
         if serializer.is_valid():
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -93,9 +81,9 @@ class PostList(APIView):
     permission_classes = [IsAuthenticated, IsStaffUser]
 
     def get(self, request):
-        """
+        '''
         Retrieve a list of all posts (accessible by staff users only).
-        """
+        '''
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -120,20 +108,20 @@ class PostsByUser(APIView):
         return Post.objects.filter(user=user, todo_date=target_date).first()
 
     def get(self, request):
-        """
+        '''
         Retrieve a list of posts created by the authenticated user.
-        """
+        '''
         posts = self.get_posts_by_user(request)
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(request_body=PostCreateSerializer)
     def post(self, request):
-        """
+        '''
         Create a new post for the authenticated user.
-        """
+        '''
         user = self.get_user(request)
-        serializer = PostCreateSerializer(data=request.data, context={"user": user})
+        serializer = PostCreateSerializer(data=request.data, context={'user': user})
         if serializer.is_valid():
             post = serializer.save()  # Save with the user
             serializer = PostSerializer(post)
@@ -143,19 +131,15 @@ class PostsByUser(APIView):
     # 포스트 내용 변경 : 'todo_date' is required
     @swagger_auto_schema(request_body=PostCreateSerializer)
     def put(self, request):
-        """
+        '''
         Update a post created by the authenticated user on a specific date.
-        """
+        '''
         user = self.get_user(request)
-        target_date = request.data.get("todo_date")
+        target_date = request.data.get('todo_date')
         post = self.get_post(request, target_date=target_date)
         if not post:
-            return Response(
-                {"error": "Post Not Found"}, status=status.HTTP_400_BAD_REQUEST
-            )
-        serializer = PostSerializer(
-            post, data=request.data, partial=True, context={"user": user}
-        )
+            return Response({'error': 'Post Not Found'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = PostSerializer(post, data=request.data, partial=True, context={'user': user})
         if serializer.is_valid():
             post = serializer.save()
             post_serializer = PostSerializer(post)
@@ -166,17 +150,14 @@ class PostsByUser(APIView):
     # 포스트 삭제 : 'todo_date' is required
     @swagger_auto_schema(request_body=PostDeleteSerializer)
     def delete(self, request):
-        """
+        '''
         Delete a post created by the authenticated user on a specific date.
-        """
+        '''
         user = self.get_user(request)
-        target_date = request.data.get("todo_date")
+        target_date = request.data.get('todo_date')
         post = self.get_post(request, target_date=target_date)
         if not post:
-            return Response(
-                {"error": "Post does not exist!"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+            return Response({'error': 'Post does not exist!'}, status=status.HTTP_404_NOT_FOUND,)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -190,16 +171,13 @@ class ToDoView(APIView):
 
     @swagger_auto_schema(request_body=ToDoCreateSerializer)
     def post(self, request, post_id):
-        """
+        '''
         Create a new to-do item for a specific post.
-        """
+        '''
         post = self.get_post(post_id)
 
         if request.user != post.user:
-            return Response(
-                {"error": "You do not have permission to add a todo to this post."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
+            return Response({'error': 'You do not have permission to add a todo to this post.'}, status=status.HTTP_403_FORBIDDEN,)
 
         serializer = ToDoCreateSerializer(data=request.data)
         if serializer.is_valid():
@@ -212,11 +190,11 @@ class ToDoView(APIView):
         return Response(serializer.errors, status=status.HTTP_200_BAD_REQUEST)
 
     def get(self, request, post_id):
-        """
+        '''
         Retrieve the to-do items for a specific post.
-        """
+        '''
         post = self.get_post(post_id)
-        todos = post.items.all().order_by("created_at")  # ascending order
+        todos = post.items.all().order_by('created_at')  # ascending order
         serializer = ToDoSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -230,16 +208,14 @@ class ToDoEdit(APIView):
 
     @swagger_auto_schema(request_body=ToDoEditSerializer)
     def put(self, request, post_id, todo_id):
-        """
+        '''
         Update a to-do item for a specific post.
-        """
+        '''
         post = self.get_post(post_id)
         try:
             todo = post.items.get(id=todo_id)
         except ToDo.DoesNotExist:
-            return Response(
-                {"error": "Todo item not found."}, status=status.HTTP_404_NOT_FOUND
-            )
+            return Response({'error': 'Todo item not found.'}, status=status.HTTP_404_NOT_FOUND)
         serializer = ToDoSerializer(todo, data=request.data, partial=True)
 
         if serializer.is_valid():
@@ -251,16 +227,14 @@ class ToDoEdit(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, post_id, todo_id):
-        """
+        '''
         Delete a to-do item for a specific post.
-        """
+        '''
         post = self.get_post(post_id)
         try:
             todo = post.items.get(id=todo_id)
         except ToDo.DoesNotExist:
-            return Response(
-                {"error": "Todo item not found."}, status=status.HTTP_404_NOT_FOUND
-            )
+            return Response({'error': 'Todo item not found.'}, status=status.HTTP_404_NOT_FOUND)
         todo.delete()
         post.update_progress()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -291,44 +265,37 @@ class Spotify(APIView):
 
     @swagger_auto_schema(query_serializer=SpotifyQuerySerializer)
     def get(self, request, post_id):
-        """
+        '''
         Get a list of songs based on the query parameter from Spotify API.
-        """
-        query = request.query_params.get("query", None)
+        '''
+        query = request.query_params.get('query', None)
         if not query:
-            return Response(
-                {"error": "Query parameter is required!"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+            return Response({'error': 'Query parameter is required!'}, status=status.HTTP_400_BAD_REQUEST,)
 
         try:
             post = self.get_post(post_id)
-            results = self.sp.search(q=query, type="track", limit=50, market="KR")
+            results = self.sp.search(q=query, type='track', limit=50, market='KR')
             tracks = []
-            for track in results["tracks"]["items"]:
+            for track in results['tracks']['items']:
                 track_data = {
-                    "album": track["album"]["name"],
-                    "release_date": track["album"]["release_date"],
-                    "singer": track["artists"][0]["name"],
-                    "title": track["name"],
-                    "song_url": track.get("preview_url", None),
+                    'album': track['album']['name'],
+                    'release_date': track['album']['release_date'],
+                    'singer': track['artists'][0]['name'],
+                    'title': track['name'],
+                    'song_url': track.get('preview_url', None),
                 }
                 serializer = SpotifySearchSerializer(data=track_data)
                 if serializer.is_valid():
                     tracks.append(serializer.validated_data)
                 else:
-                    return Response(
-                        serializer.errors, status=status.HTTP_400_BAD_REQUEST
-                    )
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             return Response(tracks, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @swagger_auto_schema(request_body=SongCreateSerializer)
     def post(self, request, post_id):
-        """
+        '''
         Add a new song to the post.
         data = {
             'album': 'abc',
@@ -337,7 +304,7 @@ class Spotify(APIView):
             'title': 'akdjkj',
             'song_url':'url',
         }
-        """
+        '''
         post = self.get_post(post_id=post_id)
         serializer = SongCreateSerializer(data=request.data)
 
@@ -346,9 +313,7 @@ class Spotify(APIView):
             current_song = self.get_current_song(post_id=post_id)
             if current_song:
                 # update the current song
-                serializer = SpotifySerializer(
-                    current_song, data=request.data, partial=True
-                )
+                serializer = SpotifySerializer(current_song, data=request.data, partial=True)
                 if serializer.is_valid():
                     song = serializer.save()
                     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -362,15 +327,12 @@ class Spotify(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, post_id):
-        """
+        '''
         Delete the current song from the post.
-        """
+        '''
         current_song = self.get_current_song(post_id=post_id)
         if not current_song:
-            return Response(
-                {"error": "No song exists"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+            return Response({'error': 'No song exists'}, status=status.HTTP_404_NOT_FOUND,)
         current_song.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -388,16 +350,13 @@ class MusicView(APIView):
         return post.music
 
     def get(self, request, post_id):
-        """
+        '''
         Get the currently registered song for the post.
-        """
+        '''
         song = self.get_current_song(post_id)
         # 해당 post_id 로 생성된 timer가 없을 경우
         if not song:
-            return Response(
-                {"error": "Song not found for the post."},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+            return Response({'error': 'Song not found for the post.'}, status=status.HTTP_404_NOT_FOUND,)
         serializer = SpotifySerializer(song)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -412,8 +371,8 @@ class TimerView(APIView):
     def get(self, request, post_id):
         post = self.get_post(post_id)
         # 해당 post_id 로 생성된 timer가 없을 경우
-        if not hasattr(post, "timer"):
-            raise NotFound(detail="Timer not found for this post.")
+        if not hasattr(post, 'timer'):
+            raise NotFound(detail='Timer not found for this post.')
 
         timer = post.timer
         timer.update_duration()
@@ -432,19 +391,17 @@ class TimerView(APIView):
     # timer reset/pause/restart
     @swagger_auto_schema(request_body=TimerActionSerializer)
     def patch(self, request, post_id):
-        action = request.data.get("action", None)
+        action = request.data.get('action', None)
         post = self.get_post(post_id)
         timer = post.timer
-        if action == "pause":
+        if action == 'pause':
             timer.pause()
-        elif action == "restart":
+        elif action == 'restart':
             timer.restart()
-        elif action == "reset":
+        elif action == 'reset':
             timer.reset()
         else:
-            return Response(
-                {"error": "Invalid action"}, status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response({'error': 'Invalid action'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(TimerSerializer(timer).data, status=status.HTTP_200_OK)
 
     def delete(self, request, post_id):
